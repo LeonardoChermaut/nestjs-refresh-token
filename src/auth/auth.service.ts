e449d3c2e454f3bb2f2d144a1d0674d808f02205
Leonardo Chermaut <chermauts@hotmail.com>
2023-08-16 19:03:45 -0300

    feat: added sigin auth method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '@src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt';
@@ -30,7 +30,24 @@ export class AuthService {
     return tokens;
   }
 
-  // signinLocal() {}
+  async signinLocal(dto: AuthDto) {
+    const user = await this.prismaService.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (!user) {
+      throw new ForbiddenException('Access Denied');
+    }
+
+    const passwordMatch = await bcrypt.compare(dto.password, user.hash);
+    if (!passwordMatch) {
+      throw new ForbiddenException('Access Denied');
+    }
+
+    const tokens = await this.getTokens(user.id, user.email);
+    await this.updateRefreshToken(user.id, tokens.refresh_token);
+
+    return tokens;
+  }
 
   // logout() {}
 
